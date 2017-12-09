8f4e2ed8c701124e92cd77c40749d8f269770ef6
Smile <lovelyeu@hanmail.net>
2017-12-09 23:52:38 +0900

    BJ p1260 DFS BFS

---
+++ AlgorithmEx/src/baekjoon/p1/p1260/Main.java
@@ -0,0 +1,20 @@
+package baekjoon.p1.p1260;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
+public class Main {
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) {		
+		
+		
+		PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+		
+		out.println();
+		out.flush();
+		out.close();
+	}
+}
