6848ebfec6d3a7eeeac5e34100e5564c627d9565
Smile <lovelyeu@hanmail.net>
2017-12-16 03:08:05 +0900

    BJ 1260 DFS

--- AlgorithmEx/src/baekjoon/p1/p1260/Main.java
+++ AlgorithmEx/src/baekjoon/p1/p1260/Main.java
@@ -2,19 +2,58 @@ package baekjoon.p1.p1260;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.util.Queue;
 
-public class Main {
+public class Main {	
 	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	public static void main(String[] args) {		
+	private static PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
 		
+	private static Queue<Integer> queue;
+	
+	private static boolean [][] lines;
+	private static boolean [] dfsVisit;
+	private static boolean [] bfsVisit;
+	
+	public static void main(String[] args) throws IOException {		
+		String[] data = br.readLine().split(" "); 
+		int n = Integer.parseInt(data[0]);
+		int m = Integer.parseInt(data[1]);
+		int v = Integer.parseInt(data[2]);
 		
-		PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+		lines = new boolean[n+1][n+1];
+		dfsVisit = new boolean[n+1];
+		bfsVisit = new boolean[n+1];
 		
-		out.println();
+		for(int i=1; i<=n; i++) {
+			data = br.readLine().split(" ");
+			int idx1 = Integer.parseInt(data[0]);
+			int idx2 = Integer.parseInt(data[1]);
+			lines[idx1][idx2] = lines[idx2][idx1] = true;
+		}
+
+		dfs(v);
+		bfs(v);
 		out.flush();
 		out.close();
 	}
+
+	private static void dfs(int v) {		
+		dfsVisit[v] = true;		
+		
+		out.print(v + " ");		
+		for(int i=1; i<lines[v].length; i++) {
+			if(lines[v][i] && !dfsVisit[i]){
+				dfs(i);
+			}
+		}
+	}
+
+	private static void bfs(int v) {
+		
+	}
+
 }
